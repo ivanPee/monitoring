38ba0ece8b05937149908f9a4885ecb8367b7791
Ivan Patricio <105693357+ivanPee@users.noreply.github.com>
2025-05-24 13:16:30 +0800

    Update buzzer.py

--- buzzer.py
+++ buzzer.py
@@ -9,8 +9,8 @@ GPIO.setup(BUZZER_PIN, GPIO.OUT)
 # Beep 5 times
 for _ in range(5):
     GPIO.output(BUZZER_PIN, GPIO.HIGH)  # Turn buzzer on
-    time.sleep(0.2)
+    time.sleep(3)
     GPIO.output(BUZZER_PIN, GPIO.LOW)   # Turn buzzer off
-    time.sleep(0.2)
+    time.sleep(3)
 
 GPIO.cleanup()
