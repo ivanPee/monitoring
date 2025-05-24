803ac78524d8cbc1428f80623a1c4e3d73b4027b
Ivan Patricio <105693357+ivanPee@users.noreply.github.com>
2025-05-24 13:11:39 +0800

    Create buzzer.py

---
+++ buzzer.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+BUZZER_PIN = 18  # GPIO pin used
+
+GPIO.setmode(GPIO.BCM)       # Use Broadcom pin-numbering
+GPIO.setup(BUZZER_PIN, GPIO.OUT)
+
+# Beep 5 times
+for _ in range(5):
+    GPIO.output(BUZZER_PIN, GPIO.HIGH)  # Turn buzzer on
+    time.sleep(0.2)
+    GPIO.output(BUZZER_PIN, GPIO.LOW)   # Turn buzzer off
+    time.sleep(0.2)
+
+GPIO.cleanup()
